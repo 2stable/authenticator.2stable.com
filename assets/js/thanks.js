d2d23774041cfe083aef503b5a0fb48c32d63389
Alex Vera <69604865+c128128@users.noreply.github.com>
2025-05-29 12:28:17 +0200

    update

---
+++ assets/js/thanks.js
@@ -0,0 +1,1 @@
+function thankYou(){document.getElementById("helpful").innerHTML='<div class="py-2">Thank You!</div>'}function needImprove(){document.getElementById("helpful").innerHTML='<div><p>How could it be better?</p><a href="mailto:support@2stable.com"><div class="btn btn-outline-primary"><i class="bi bi-envelope pe-2"></i>Send us an email</div></a></div>'}
